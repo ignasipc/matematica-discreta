e0f61c0476d8f1abd57e70e92e40efd50a0c935f
AzPlayer <asierzubillagallabres@protonmail.com>
2024-06-16 18:52:54 +0200

    nombres 2 electric boogaloo

--- Entrega.java
+++ Entrega.java
@@ -41,8 +41,8 @@ import java.util.stream.Stream;
  * Podeu fer aquesta entrega en grups de com a màxim 3 persones, i necessitareu com a minim Java 10.
  * Per entregar, posau a continuació els vostres noms i entregau únicament aquest fitxer.
  * - Nom 1:Ignasi Paredes Casasnovas
- * - Nom 2:Asier zubillaga llabres
- * - Nom 3:
+ * - Nom 2:Elena Morey Martin
+ * - Nom 3:Asier zubillaga llabres 
  *
  * L'entrega es farà a través d'una tasca a l'Aula Digital que obrirem abans de la data que se us
  * hagui comunicat i vos recomanam que treballeu amb un fork d'aquest repositori per seguir més
